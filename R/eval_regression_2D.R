e0777975ee30988a4cc222265ea7a0714a852254
Oliver4242 <oliver.duerr@gmail.com>
2022-08-03 15:01:04 +0200

    Added data and evaluation for 2D

---
+++ R/eval_regression_2D.R
@@ -0,0 +1,117 @@
+#############################################################
+library(keras)
+library(tensorflow)
+library(tfprobability)
+library(ggplot2)
+library(rstan)
+library(loo)
+library(readr)
+source('R/eval_utils_multi.R')
+
+dir_name = 'R/runs/gpu_2D_F1F2_Epo_100000_M_50_T_10/'
+reps = 5
+#Make Standard Plots and save to dir_name
+df2 = make_plots_and_stats(dir_name = dir_name, reps=reps)
+#0.68 (0.51, 0.86) 90$\perc$ CI
+
+#### Loading of Data and MCMC Samples #######
+load('data/2D.rdata')
+x_r
+y_r
+N = 6L # number data points
+P = 2L #number of predictors
+D = P + 2L  #Dimension (number of weights) predictors + intercept + sigma
+#Loading MCMC SAMPLES --> posts_mcmc
+load('mcmc/2D/2D_mcmc_samples.rda')
+if (FALSE){ #Time consuming MCMC
+  dl = list(N=N,x=x_r,y=y_r, P = P)
+  samples = stan(file="mcmc/2D/tmvi_multiple_lr_sigma.stan", data=dl, iter = 24000)
+  samples
+  #traceplot(samples)
+  posts_mcmc = extract(samples)
+  save(posts_mcmc, file = 'mcmc/2D/2D_mcmc_samples.rda')
+}
+
+#### Loading of samples from variational posterior
+load('R/runs/gpu_2D_F1F2_Epo_100000_M_50_T_10/samples_1.rda')
+w = samples$w
+
+
+
+####### 
+# For paper
+sigma = tf$math$softplus(w[,P+2])$numpy()
+TT = 1000 #Subsampling to avoid too large plots
+df = data.frame(beta1 = posts_mcmc$w[1:TT,1], 
+                beta2=posts_mcmc$w[1:TT,2], 
+                sigma=posts_mcmc$sigma[1:TT],
+                intercept = posts_mcmc$b[1:TT],
+                type='MCMC')
+df = rbind(df, data.frame(
+  beta1 = w[1:TT,2], 
+  beta2=w[1:TT,3], 
+  sigma=sigma[1:TT],
+  intercept  =w[1:TT,1],
+  type='BF-VI'))
+library(ggplot2)
+library(GGally)
+p = ggpairs(df, 
+            aes(color = type, alpha=0.5), 
+            columns = c(1,2,3,4),
+            columnLabels = c('beta1', "beta2", "sigma", 'intercept')
+) + 
+  scale_color_manual(values = c('blue', 'red')) + 
+  scale_fill_manual(values = c('blue', 'red')) 
+  #labs(title = dir_name)
+p = p + theme_bw() + theme(
+  panel.grid.major=element_blank(),
+  panel.grid.minor=element_blank()
+)
+p
+ggsave(p, filename = 'figures/2D_pairs.pdf', width = 7, height = 7)
+#Attention Manual relabeling has been done
+#ggsave(p, filename = '~/Dropbox/Apps/Overleaf/bernvi/images/2D_pairs.pdf',width = 7, height=7)
+
+###### Pairs Plot for the appendix ########
+#########  Mean Field Gaussian ########
+# Mean field gaussian
+# https://colab.research.google.com/drive/1FKnZ-0x5yBlM3-4p1GzQgW35JhfUwxix
+b = read_csv("R/runs/2D_MF_Gauss/2D_MFG_intercept_samples.csv.gz")
+w = read_csv("R/runs/2D_MF_Gauss/2D_MFG_w_samples.csv.gz")
+s = read_csv("R/runs/2D_MF_Gauss/2D_MFG_sigma_samples.csv.gz")
+TT = 1000
+df = data.frame(beta1 = posts_mcmc$w[1:TT,1], 
+                beta2=posts_mcmc$w[1:TT,2], 
+                sigma=posts_mcmc$sigma[1:TT],
+                intercept = posts_mcmc$b[1:TT],
+                type='MCMC')
+
+df = rbind(df, data.frame(
+  beta1 = w$w1[1:TT], 
+  beta2 = w$w2[1:TT], 
+  sigma= s$b[1:TT],
+  intercept  = b$b[1:TT],
+  type='Gauss-MF'))
+library(ggplot2)
+library(GGally)
+p = ggpairs(df, 
+            aes(color = type, alpha=0.5), 
+            columns = c(1,2,3,4),
+            columnLabels = c('beta1', "beta2", "sigma", 'intercept')
+) + 
+  scale_color_manual(values = c('blue', 'red')) + 
+  scale_fill_manual(values = c('blue', 'red'))
+p = p + theme_bw() + theme(
+  panel.grid.major=element_blank(),
+  panel.grid.minor=element_blank()
+)
+p
+ggsave(p, filename = 'figures/2D.pairs.gauss_vs.mcmc.pdf', width = 7, height = 7)
+#ggsave(p, filename = '~/Dropbox/Apps/Overleaf/bernvi/images/2D.pairs.gauss_vs.mcmc.pdf',width = 7, height=7)
+
+  
+
+
+
+
+
---
+++ R/eval_utils_multi.R
@@ -0,0 +1,167 @@
+get_losses_metrics = function(dir_name_r, reps, num_boot = 1000){
+  loss_hists = all_samples = {}
+  KL_est = KL_var = ELBO_est = ELBO_var = k_hat_var = upper_slope_loss = lower_slope_loss = evidence_est = evid_var = losses_sd = losses_mean = k_hats = rep(NA, reps)
+  k_CI = matrix(rep(NA, 3*reps), ncol = 3)
+  evid_CI = matrix(rep(NA, 3*reps), ncol = 3)
+  ELBO_CI = matrix(rep(NA, 3*reps), ncol = 3)
+  KL_CI = matrix(rep(NA, 3*reps), ncol = 3)
+  for (rep in 1:reps) {
+    load(file.path(dir_name_r, paste0('loss_hist_',rep,'.rda')))
+    loss_hists[[rep]] = loss_hist   
+    load(file.path(dir_name_r, paste0('samples_',rep,'.rda')))
+    all_samples[[rep]] = samples  
+    
+    #Estimation of k_hat
+    log_joint = samples$LLs + samples$L_priors
+    log_ratios = as.vector(log_joint - samples$log_qs)
+    res_psis = psis(log_ratios)
+    k_hats[rep] = res_psis$diagnostics$pareto_k
+    
+    #See e.g. https://bochang.me/blog/posts/iwae/ 
+    evidence_est[rep] = pomp::logmeanexp(log_ratios)
+   
+    ELBO_est[rep] = mean(log_ratios)
+    
+     
+    #Bootstrap CI
+    boot_diff = boot_evid = boot_elbo = boot_khat = rep(NA, num_boot)
+    print(rep)
+    for (i in 1:num_boot){
+      idx = sample(1:length(log_ratios), replace = TRUE)
+      boot_khat[i] = psis(log_ratios[idx])$diagnostics$pareto_k
+      boot_evid[i] = pomp::logmeanexp(log_ratios[idx])
+      boot_elbo[i] =  mean(log_ratios[idx])
+      boot_diff[i] =  boot_evid[i] - boot_elbo[i] 
+    }
+    k_CI[rep,] = quantile(boot_khat, c(0.05, 0.5, 0.95))
+    k_hat_var[rep] = var(boot_khat)
+    
+    ELBO_CI[rep,] = quantile(boot_elbo, c(0.05, 0.5, 0.95))
+    ELBO_var[rep] = var(boot_elbo)
+    
+    evid_CI[rep,] = quantile(boot_evid, c(0.05, 0.5, 0.95))
+    evid_var[rep] = var(boot_evid)
+    
+    KL_CI[rep,] = quantile(boot_diff, c(0.05, 0.5, 0.95))
+    KL_var[rep] = var(boot_diff)
+    #plot(density(boot_elbo), xlim=c(-28.7,-28.5), main=rep)
+    #lines(density(boot_evid), col='red')
+    
+    N = length(loss_hist)
+    sd(loss_hist[(N-1e3):N]) 
+    losses_mean[rep] = mean(loss_hist[(N-1e3):N])
+    losses_sd[rep] = sd(loss_hist[(N-1e3):N])
+    
+    xd = 80000:100000
+    yd = loss_hists[[rep]][xd]
+    lower_slope_loss[rep] = confint(lm(yd ~ xd))[2,1]
+    upper_slope_loss[rep] = confint(lm(yd ~ xd))[2,2]
+    
+  }#Reps
+  df = data.frame(
+    k_hats=k_hats,
+    k_hat_q05  =  k_CI[,1],
+    k_hat_q5  =  k_CI[,2],
+    k_hat_q95  =  k_CI[,3],
+    k_hat_var = k_hat_var,
+    losses_mean = losses_mean,
+    losses_sd = losses_sd,
+    ELBO_est = ELBO_est,
+    ELBO_q05 = ELBO_CI[,1],
+    ELBO_q5 = ELBO_CI[,2],
+    ELBO_q95 = ELBO_CI[,3],
+    ELBO_var = ELBO_var,
+    evidence_est = evidence_est,
+    evid_q05 = evid_CI[,1],
+    evid_q5 = evid_CI[,2],
+    evid_q95 = evid_CI[,3],
+    evid_var = evid_var,
+    KL_est = KL_est,
+    KL_q05 = KL_CI[,1],
+    KL_q5 = KL_CI[,2],
+    KL_q95 = KL_CI[,3],
+    KL_var = KL_var,
+    lower_slope_loss = lower_slope_loss,
+    upper_slope_loss = upper_slope_loss
+  )
+  df$reps = 1:reps
+  return(list(loss_hists=loss_hists, df=df))
+}
+
+
+make_plots_and_stats = function(dir_name, reps){
+  print(paste0("Creating plots in ", dir_name))
+  rets = get_losses_metrics(dir_name, reps=reps, num_boot = 1000)
+  loss_hists = rets[['loss_hists']]
+  df = rets[['df']]
+  
+  ## Plot of the loss function
+  N_Loss = length(loss_hists[[1]])
+  idx = seq(1, N_Loss, length.out = 1000) 
+  l = quantile(loss_hists[[1]][idx], 0)
+  u = quantile(loss_hists[[1]][idx], 0.85)
+  #plot(loss_hists[[1]][idx], type='l', main=dir_name, col='skyblue', ylim=c(25,45))
+  pdf(file.path(dir_name, 'loss.pdf'))
+  plot(idx, loss_hists[[1]][idx], type='l', 
+       main=dir_name, col='skyblue', ylim=c(l - 0.05*abs(l),u),
+       xlab = 'Epochs',
+       ylab='loss (ELBO)',
+       cex.main=0.65
+  )
+  abline(h=-df$evidence_est[1],col='skyblue')
+  for (rep in 2:reps){
+    #lines(loss_hists[[rep]][idx], col=rep)  
+    lines(idx,loss_hists[[rep]][idx], col=rep)  
+    abline(h=-df$evidence_est[rep],col=rep)
+  }
+  dev.off()
+  
+  ## k-hat plot
+  k_bar = mean(df$k_hats)
+  var_boot = mean(df$k_hat_var)
+  var_runs = var(df$k_hats)
+  var_rubin = var_boot + (1 + 1/reps) * var_runs
+  k_rubin_lower = k_bar - qnorm(0.95)*sqrt(var_rubin)
+  k_rubin_upper = k_bar + qnorm(0.95)*sqrt(var_rubin)
+  
+  df2 = data.frame(x=0, ymin=k_rubin_lower, ymax=k_rubin_upper, y=k_bar)
+  p = ggplot(df, aes(x=reps)) + 
+    geom_errorbar(aes(y=k_hat_q5, ymin=k_hat_q05, ymax=k_hat_q95),col='red') + geom_point(aes(y=k_hat_q5)) + 
+    ylab('k_hat') +
+    geom_errorbar(data=df2, mapping=aes(x=x, ymin=ymin, ymax=ymax)) +
+    geom_point(data=df2, aes(x=x, y=k_bar)) +
+    labs(title = dir_name, 
+         subtitle='Bootstrap (red) and Rubins (black) CI (90)')
+  p
+  ggsave(file.path(dir_name, 'k_hat.pdf'), p) 
+  
+  ggplot(df, aes(x=reps, y=evid_q5)) + 
+    geom_errorbar(aes(ymin=evid_q05, ymax=evid_q95),col='red') + geom_point() + 
+    ylab('Evidence') +
+    labs(title = dir_name, 
+         subtitle='Bootstrap CI (90) for 5 Different Runs 1K Bootstrap Samples')
+  ggsave(file.path(dir_name, 'evidence.pdf')) 
+  
+  ggplot(df, aes(x=reps, y=ELBO_q5)) + 
+    geom_errorbar(aes(ymin=ELBO_q05, ymax=ELBO_q95),col='red') + geom_point() + 
+    ylab('ELBO') +
+    labs(title = dir_name, 
+         subtitle='Bootstrap CI (90) for 5 Different Runs 1K Bootstrap Samples')
+  ggsave(file.path(dir_name, 'elbo.pdf')) 
+  
+  ggplot(df, aes(x=reps, y=KL_q5)) + 
+    geom_errorbar(aes(ymin=KL_q05, ymax=KL_q95),col='red') + geom_point() + 
+    ylab('KL(q||post) EVIDENCE - ELBO') +
+    labs(title = dir_name, 
+         subtitle='Bootstrap CI (90) for 5 Different Runs 1K Bootstrap Samples')
+  ggsave(file.path(dir_name, 'kl.pdf'))
+  
+  return(
+    data.frame(
+      k_bar,
+      k_rubin_lower,
+      k_rubin_upper,
+      dir_name
+    )
+  )
+} 
