d09ab0ae1c44343d3f4deda9ea5a92c58a9c45f6
Beate Sick <sick@zhaw.ch>
2022-08-09 15:55:57 +0200

    added reps=5

--- R/eval_Diamonds.R
+++ R/eval_Diamonds.R
@@ -9,6 +9,7 @@ library(rstan)
 source('R/eval_utils_multi.R')
 
 #Loading of cached data
+reps=5
 dir_name = 'R/runs/Diamonds'
 dir_name = 'R/runs/cpu_DIAMONDS_F1F2_Epo_100000_M_50_T_10/'
 df2 = make_plots_and_stats(dir_name = dir_name, reps=reps)
@@ -21,7 +22,7 @@ if (FALSE){
   my_pdb <- pdb_local(path='~/Documents/workspace/posteriordb')
   po <- posteriordb::posterior("diamonds-diamonds", my_pdb)
   draws = get_data(po)
-  reference_posterior_draws_info(po)  
+  reference_posterior_draws_info(po)
   posteriordb::stan_code(po)
   rpd <- reference_posterior_draws(po)
   posterior::summarize_draws(rpd)
@@ -56,7 +57,7 @@ lines(density(chef_mcmc$Intercept), col='pink', lwd=2)
 #hist(chef_mcmc$Intercept, xlim=c(5, 10))
 #lines(density(w[,1]), col='skyblue', lwd=2)
 abline(v=ml$coefficients[1], col='darkgreen', lwd=3)
-legend('topleft', legend = c('VI', 'MCMC', 'ML'), lty=c(1,1), 
+legend('topleft', legend = c('VI', 'MCMC', 'ML'), lty=c(1,1),
        col=c('skyblue', 'red', 'darkgreen'), lwd=3, bty='n')
 
 car::qqPlot(chef_mcmc$Intercept, main='MCMC Intercept')
@@ -73,11 +74,11 @@ par(mfrow = c(2,1))
 w_vi = as.data.frame(w[,1:25])
 w_vi$sigma = sigma
 names(w_vi) = c('Intercept', paste0('b',1:24), 'Sigma')
-boxplot(w_vi[1:sub_sample,], ylim=c(-10,15), main='BF-VI', las=2, pch=21) 
+boxplot(w_vi[1:sub_sample,], ylim=c(-10,15), main='BF-VI', las=2, pch=21)
 points(ml$coefficients[1:25], col='red', pch=4)
 d = cbind(chef_mcmc$Intercept, chef_mcmc[1:24], chef_mcmc$sigma)
 names(d) = c('Intercept', paste0('b',1:24), 'Sigma')
-boxplot(d[1:sub_sample,], ylim=c(-10,15), main='MCMC', las=2) 
+boxplot(d[1:sub_sample,], ylim=c(-10,15), main='MCMC', las=2)
 points(ml$coefficients[1:25], col='red', pch=4)
 par(mfrow = c(1,1))
 dev.off()
