f0f36f8d8dc26699c5b8ca168303def6cd238dad
Beate Sick <sick@zhaw.ch>
2022-08-10 13:02:46 +0200

    plots update, del one line

--- R/eval_8Schools_ncp.R
+++ R/eval_8Schools_ncp.R
@@ -25,18 +25,18 @@ if (FALSE){
   fit <- sampling(m, data=data, iter = 10000, seed=194838,control=list(adapt_delta=0.9))
   posts_mcmc = extract(fit)
   save(posts_mcmc, file = '~/Dropbox/__ZHAW/__Projekte_Post_ZHAH/shared_Oliver_Beate/tmvi/runs/8SCHOOLS_NCP_Run1/mcmc_samples.rda')
-  
+
   #Using Stan AUVI
   fit_vb=vb(m,  data=data, iter=1e6,output_samples=1e5,tol_rel_obj=0.001)
   vb_sample=extract(fit_vb)
   vb_samples = data.frame(vb_sample$mu, vb_sample$tau, vb_sample$theta)
-  error_mean_sigma(mcmc_samples, vb_samples)  
-  #L2_mu   L2_sig 
-  #1.892609 1.207052 
-  
+  error_mean_sigma(mcmc_samples, vb_samples)
+  #L2_mu   L2_sig
+  #1.892609 1.207052
+
   #Second run
-  #L2_mu   L2_sig 
-  #1.985089 1.227350 
+  #L2_mu   L2_sig
+  #1.985089 1.227350
   psis(vb_sample)
 }
 
@@ -44,17 +44,16 @@ load('R/runs/gpu_8SCHOOLS_F1F2_Epo_100000_M_50_T_10/samples_1.rda')
 wws = samples$w
 theta_tilde_vi = wws[,3:10]
 mu = wws[,2]
-tau = exp(0.1 * wws[,1]) 
+tau = exp(0.1 * wws[,1])
 theta = mu + tau*theta_tilde_vi
 
-theta_tilde_vi = ww[,3:10]
 pdf('figures/8schools_ncp.pdf')
 par(mfrow=(c(1,2)))
-boxplot(as.matrix(posts_mcmc$theta_tilde), main='MCMC', 
-        #sub=dir_name,ylim=c(-4.5,4.5), 
+boxplot(as.matrix(posts_mcmc$theta_tilde), main='MCMC',
+        #sub=dir_name,ylim=c(-4.5,4.5),
         #ylab='theta_tilde'
         )
-boxplot(as.matrix(theta_tilde_vi), main='VI',  
+boxplot(as.matrix(theta_tilde_vi), main='VI',
         #sub=dir_name,
         ylim=c(-4.5,4.5)
         #,ylab='theta_tilde'
