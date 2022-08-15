a56fa58c1cc072adc90fd7a04e1b55a173052e1d
Beate Sick <sick@zhaw.ch>
2022-08-15 13:36:28 +0200

    deleted unnecessary code junk

--- R/eval_cauchy_ablation.R
+++ R/eval_cauchy_ablation.R
@@ -23,21 +23,21 @@ apatheme=theme_bw(base_size = 22)+
         axis.text.y=element_text(size = 20),
         axis.text.x=element_text(size = 20))
 
-d =  fread("Python/Cauchy/Gauss-VI_densities.csv.gz")[-1,] 
+d =  fread("Python/Cauchy/Gauss-VI_densities.csv.gz")[-1,]
 kl_gauss = eval_kl_cauchy(d$V1, log(d$V2))
 df_p = rbind(df_plot, data.frame(M=0.5, method='Gauss-VI', x=d$V1, density=d$V2, seed=1))
-dd = df_p %>% filter(method %in% c('F1F2', 'Gauss-VI', 'MCMC')) %>% 
+dd = df_p %>% filter(method %in% c('F1F2', 'Gauss-VI', 'MCMC')) %>%
   filter(M %in% c(0.5,2,6,10,30,50,0)) %>% #0.5 is Gauss, 0 is MCMC
   #group_by(method) %>%
   #mutate(num_rows=n()) %>%
   #sample_frac(0.4, weight=num_rows) %>%
-  #ungroup %>% 
-  filter(as.numeric(seed) < 11) %>% arrange(method, factor(M), x) 
+  #ungroup %>%
+  filter(as.numeric(seed) < 11) %>% arrange(method, factor(M), x)
 
 library(dplyr)
-p = ggplot(dd) + 
-  geom_line(aes(x=x, y=density, col=factor(M)),size=0.75) + 
-  xlab(expression(xi)) + 
+p = ggplot(dd) +
+  geom_line(aes(x=x, y=density, col=factor(M)),size=0.75) +
+  xlab(expression(xi)) +
   scale_color_manual(
     name = 'Method',
     values = c(
@@ -74,25 +74,20 @@ apatheme=theme_bw(base_size = 22)+
         axis.text.x=element_text(size = 20))
 
 
-x = 3:60
-df2 = data.frame(
-  x = x,
-  y = 1.2/x
-)
 
 ## Adding Gaus #####
 df_kl = rbind(df_kl,
               data.frame(seed = 1, kl=kl_gauss, M=0.5, method='Gauss-VI', num_samples=1))
 
-dat = df_kl %>% 
-  filter(M < 61009) 
+dat = df_kl %>%
+  filter(M < 61009)
   #filter(method == 'F1F2' | method == 'Gauss-VI')
 
-gg_chauchy = ggplot(dat) + 
+gg_chauchy = ggplot(dat) +
   geom_jitter(aes(x=M, y=kl, col=method),alpha=0.9) +
-  #geom_boxplot(aes(x=M, y=kl, fill=method, group=cut_interval(x=M, length=0.5))) + 
+  #geom_boxplot(aes(x=M, y=kl, fill=method, group=cut_interval(x=M, length=0.5))) +
   #geom_line(data=df2, aes(x=x,y=y), linetype=2, size=1.2) +
-  scale_x_log10() + 
+  scale_x_log10() +
   ylab(expression(paste('KL(q(',theta,') || p(',theta,'|D))')))+
   #scale_y_log10(name=expression(paste('KL(q(',theta,') || p(',theta,'|D))'))) +
   #annotate("text", x = 20, y = 2/20, label = "~1/M", size=6) +
